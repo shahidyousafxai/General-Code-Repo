66d8672d38fcc642887751582c7484eafd8e7922
Shahid Bahadur <shahidbahadur1998@gmail.com>
2023-09-14 18:09:00 +0500

    Update checkCapsLock.js

--- checkCapsLock.js
+++ checkCapsLock.js
@@ -2,3 +2,4 @@ const enterInput = (e) => {
         const flag = e.getModifierState("CapsLock")
         console.log("-------->", flag)
     }
+// pass the above function in input inside "onMouseDown"
